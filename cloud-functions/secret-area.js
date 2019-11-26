6ff25d3fea6aed4eb6dba4373b2f19d967bd242e
Erik Maier <erikmaier80@gmail.com>
2019-11-26 23:55:42 +0100

    Updated cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,27 @@
 exports.handler = function(event, conext, callback) {
+const secretContent = `
+<h3>Welcome To The Secret WebsiteRebels Area</h3>
+<p>Here we will make <strong>your</strong> Network dreams come true</p>
+`
+
+    let body
+
+if (event.body) {
+    body = JSON.parse(event.body)
+} else {
+    body = {}
+}
+
+if (body.password == "websiterebels") {
     callback(null, {
         statusCode: 200,
-        body: "Welcome to the super secret area"
+        body: secretContent
     })
+} else {
+    callback(null, {
+        statusCode: 401
+    })
+}
+
+    
 }
